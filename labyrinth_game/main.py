7f913f638a40df8a02df6a586136581cea5e2a05
lalala-0 <vylegzhanina.alena@yandex.ru>
2025-10-19 22:35:28 +0300

    feat: add rooms constants and game state structure

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+from labyrinth_game.constants import ROOMS
+import labyrinth_game.utils as utils
+import  labyrinth_game.player_actions as pa
+
+game_state = {
+        'player_inventory': [], # Инвентарь игрока
+        'current_room': 'entrance', # Текущая комната
+        'game_over': False, # Значения окончания игры
+        'steps_taken': 0 # Количество шагов
+  }
+   
 def main():
     print("Первая попытка запустить проект!")
